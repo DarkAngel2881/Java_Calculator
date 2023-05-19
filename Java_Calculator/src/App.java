dfadbb1a15cdf68dd349be52e221c9d6c0d3e75c
DarkAngel2881 <100215802+DarkAngel2881@users.noreply.github.com>
2023-05-19 10:27:42 +0000

    first test

--- Java_Calculator/src/App.java
+++ Java_Calculator/src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        // create the calculator frame
+        JFrame main_frame = new JFrame("Calculator");
+        main_frame.setLayout(null);
+
+        // create the panel
+        JPanel main_panel = new JPanel(null);
+
+        // set sizes
+        main_frame.setSize(400, 500);
+        main_panel.setSize(400, 500);
+
     }
 }
---
+++ Java_Calculator/src/B.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class B extends Button
+{
+    public B(String text)
+    {
+
+    }
+}
