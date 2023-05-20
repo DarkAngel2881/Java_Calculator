75ffde433f9bceb67668fa5055c737db4c803a56
DarkAngel2881 <matteo2881@gmail.com>
2023-05-20 22:30:31 +0200

    Graphics added with operators and stuff

--- Java_Calculator/src/App.java
+++ Java_Calculator/src/App.java
@@ -1,11 +1,13 @@
-import java.awt.Button;
-
+import java.awt.*;
 import javax.swing.*;
 
 
 public class App {
     public static void main(String[] args) throws Exception 
-    {
+    {   
+        // Font definition
+        Font SansSerifBold = new Font("SansSerif", Font.BOLD, 30);
+
         // create the calculator frame
         JFrame main_frame = new JFrame("Calculator");
         main_frame.setLayout(null);
@@ -13,24 +15,45 @@ public class App {
         // create the panel
         JPanel main_panel = new JPanel(null);
 
-        //  create buttons
-        Bttn zero   = new Bttn("0", 100, 350, 80, 60);
-        Bttn one    = new Bttn("1", 10 , 280, 80, 60);
-        Bttn two    = new Bttn("2", 100, 280, 80, 60);
-        Bttn three  = new Bttn("3", 190, 280, 80, 60);
-        Bttn four   = new Bttn("4", 10 , 210, 80, 60);
-        Bttn five   = new Bttn("5", 100, 210, 80, 60);
-        Bttn six    = new Bttn("6", 190, 210, 80, 60);
-        Bttn seven  = new Bttn("7", 10 , 140, 80, 60);
-        Bttn eight  = new Bttn("8", 100, 140, 80, 60);
-        Bttn nine   = new Bttn("9", 190, 140, 80, 60);
+        //  create figures buttons
+        Bttn zero   = new Bttn("0", 100, 450, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn one    = new Bttn("1", 10 , 380, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn two    = new Bttn("2", 100, 380, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn three  = new Bttn("3", 190, 380, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn four   = new Bttn("4", 10 , 310, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn five   = new Bttn("5", 100, 310, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn six    = new Bttn("6", 190, 310, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn seven  = new Bttn("7", 10 , 240, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn eight  = new Bttn("8", 100, 240, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn nine   = new Bttn("9", 190, 240, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+
+        // create operators buttons
+        Bttn eq     = new Bttn("=" , 280, 450, 80, 60, Color.green   , Color.white, SansSerifBold);
+        Bttn pl_min = new Bttn("±" , 10 , 450, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn comma  = new Bttn("," , 190, 450, 80, 60, Color.darkGray, Color.white, SansSerifBold);
+        Bttn plus   = new Bttn("+" , 280, 380, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn minus  = new Bttn("-" , 280, 310, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn per    = new Bttn("×" , 280, 240, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn div    = new Bttn("÷" , 280, 170, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn prc    = new Bttn("%" , 190, 170, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn brks   = new Bttn("()", 100, 170, 80, 60, Color.darkGray, Color.green, SansSerifBold);
+        Bttn del    = new Bttn("C" , 10 , 170, 80, 60, Color.darkGray, Color.red  , SansSerifBold);
+
+        // line for aesthetics
+        JPanel line = new JPanel(null);
+        line.setBounds(10, 155, 350, 1);
+        line.setBackground(Color.lightGray);
 
         // set sizes
-        main_frame.setSize(395, 470);
-        main_panel.setSize(395, 470);
+        main_frame.setSize(385, 560);
+        main_panel.setSize(385, 560);
+
+        // set panel color background
+        main_panel.setBackground(Color.black);
         
         // add everithing in the panel and frame
         main_frame.add(main_panel);
+        main_panel.add(line);
         main_panel.add(zero);
         main_panel.add(one);
         main_panel.add(two);
@@ -41,6 +64,17 @@ public class App {
         main_panel.add(seven);
         main_panel.add(eight);
         main_panel.add(nine);
+        main_panel.add(plus);
+        main_panel.add(minus);
+        main_panel.add(per);
+        main_panel.add(div);
+        main_panel.add(eq);
+        main_panel.add(pl_min);
+        main_panel.add(comma);
+        main_panel.add(prc);
+        main_panel.add(brks);
+        main_panel.add(del);
+
 
         // make it visible
         main_frame.setVisible(true);
--- Java_Calculator/src/Bttn.java
+++ Java_Calculator/src/Bttn.java
@@ -1,11 +1,15 @@
-import javax.swing.*;
+//import javax.swing.*;
 import java.awt.*;
 
 public class Bttn extends Button
 {   
-    public Bttn(String t, int x, int y, int w, int h)
+    public Bttn(String t, int x, int y, int w, int h, Color c, Color tc, Font f)
     {
         setLabel(t);
         setBounds(x, y, w, h);
+        setBackground(c);
+        setForeground(tc);
+        setFocusable(false);
+        setFont(f);
     }
 }
